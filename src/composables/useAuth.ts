1a4b01b468e23ed1887e86dfd1a11b6d1a1fb29c
Sverre Skuland <sverre.skuland@gmail.com>
2022-12-01 10:26:51 +0100

    Fix sign out logic.

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,2 +1,9 @@
 export const useAuth = () => useSupabaseUser();
-export const logout = () => useSupabaseClient().auth.signOut();
+export const logout = async () => {
+    await useSupabaseClient().auth.signOut();
+    const accessToken = useCookie('sb-access-token');
+    const refreshToken = useCookie('sb-refresh-token');
+
+    accessToken.value = null;
+    refreshToken.value = null;
+};
