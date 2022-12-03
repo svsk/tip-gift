419b7a1f834884f54900d4a0de554421d39513fd
Sverre Skuland <sverre.skuland@gmail.com>
2022-12-03 11:01:34 +0100

    Add shadow to cards.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="rounded bg-gray-800 p-6">
+    <div class="rounded bg-gray-800 p-6 drop-shadow-xl">
         <slot />
     </div>
 </template>
--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -16,7 +16,7 @@ const emit = defineEmits(['update:modelValue']);
                 v-if="modelValue"
                 class="fixed w-full h-full bg-opacity-30 bg-black top-0 left-0 flex flex-col items-center justify-center p-6"
             >
-                <Card @click.stop class="w-full max-w-[450px] p-0 drop-shadow-xl">
+                <Card @click.stop class="w-full max-w-[450px] p-0">
                     <div v-if="$slots.title" class="p-4">
                         <h2 class="font-medium text-lg">
                             <slot name="title" />
