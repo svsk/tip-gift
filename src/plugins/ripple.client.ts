8afc0226e73865c972b8cc54cbc7671ff021ce32
Sverre Skuland <sverre.skuland@gmail.com>
2023-09-06 17:30:15 +0200

    Add some spacing in ripple directive.

--- src/plugins/ripple.client.ts
+++ src/plugins/ripple.client.ts
@@ -3,6 +3,7 @@ export default defineNuxtPlugin((nuxtApp) => {
         mounted: (parent: HTMLElement) => {
             parent.dataset.hasRipple = 'true';
             let timerId: any = 0;
+
             parent.addEventListener('mousedown', (e: MouseEvent) => {
                 clearTimeout(timerId);
                 let ripple = parent.querySelector<HTMLDivElement>('.ripple');
@@ -11,17 +12,20 @@ export default defineNuxtPlugin((nuxtApp) => {
                     ripple.classList.add('ripple');
                     parent.appendChild(ripple);
                 }
+
                 //const ripple = e.target.querySelector('.ripple');
                 const size = parent.offsetWidth;
                 const pos = parent.getBoundingClientRect();
                 const x = e.pageX - pos.left - size;
                 const y = e.pageY - pos.top - size;
+
                 ripple.style.top = `${y}px`;
                 ripple.style.left = `${x}px`;
                 ripple.style.width = `${size * 2}px`;
                 ripple.style.height = `${size * 2}px`;
                 ripple.classList.remove('active');
                 ripple.classList.remove('start');
+
                 setTimeout(() => {
                     ripple?.classList.add('start');
                     setTimeout(() => {
@@ -29,9 +33,11 @@ export default defineNuxtPlugin((nuxtApp) => {
                     }, 0);
                 }, 0);
             });
+
             parent.addEventListener('mouseup', (e: MouseEvent) => {
                 const ripple = (e.target as HTMLElement)?.querySelector('.ripple');
                 clearTimeout(timerId);
+
                 timerId = setTimeout(() => {
                     ripple?.classList.remove('active');
                     ripple?.classList.remove('start');
