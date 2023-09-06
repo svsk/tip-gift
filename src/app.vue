1325787ad0f27c624039619de527254d1513e228
Sverre Skuland <sverre.skuland@gmail.com>
2023-09-06 13:58:33 +0200

    Upgrade packages.

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
-import { useBodyAttrs, useTagMeta } from '@unhead/vue';
-
 const embedded = useRoute().query.embedded === 'true';
 
-useTagMeta([
-    { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
-    { name: 'mobile-web-app-capable', content: 'yes' },
-    { name: 'apple-mobile-web-app-capable', content: 'yes' },
-    { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-]);
+useHead({
+    meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+    ],
+});
 
 const layoutName = embedded ? 'embed' : 'default';
 
 if (layoutName === 'embed') {
-    useBodyAttrs({
-        class: 'embedded',
+    useHead({
+        bodyAttrs: {
+            class: 'embedded',
+        },
     });
 }
 </script>
