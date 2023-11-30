701c52e54a91daa4a617dc51157210515aac18ee
Sverre Skuland <sverre.skuland@gmail.com>
2023-11-30 08:58:02 +0100

    Improve UX of user lookup.

--- src/components/UserLookup.vue
+++ src/components/UserLookup.vue
@@ -40,12 +40,13 @@ const handleUserSelected = (user: WishUser) => {
             </template>
         </Input>
 
-        <Card v-if="result.length" class="flex flex-col gap-4 p-4 absolute w-full shadow-md z-50">
+        <Card v-if="result.length" class="flex flex-col gap-2 absolute w-full shadow-md z-50 p-0 overflow-hidden">
             <User
                 v-for="user in result"
                 :key="user.Id"
+                v-ripple
                 avatar-class="scale-[0.85]"
-                class="flex gap-2 items-center"
+                class="py-2 px-4 flex gap-2 items-center cursor-pointer hover:bg-white hover:bg-opacity-10 relative"
                 :user-id="user.Id"
                 @click="() => handleUserSelected(user)"
             />
