be9a870b83a0ffc8249b99e459e41bf59fad7e9a
Sverre Skuland <sverre.skuland@gmail.com>
2024-09-26 20:23:25 +0200

    Give the checkmark in the checkbox white coloring.

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -2,21 +2,33 @@
 interface Props {
     modelValue: boolean | null;
     threeState?: boolean;
+    size?: string | null;
 }
 
 interface Emits {
     (event: 'update:modelValue', value: boolean | null): void;
 }
 
-const props = withDefaults(defineProps<Props>(), { threeState: false });
+const props = withDefaults(defineProps<Props>(), { threeState: false, size: '1em' });
 const emit = defineEmits<Emits>();
 
 const value = ref<boolean | null>(false);
 
+const handleClick = () => {
+    if (props.threeState) {
+        if (value.value === null) {
+            value.value = false;
+        } else {
+            value.value = !value.value;
+        }
+    } else {
+        value.value = !value.value;
+    }
+};
+
 watch(
     () => props.modelValue,
     () => {
-        console.log('setting new value', props.modelValue);
         value.value = props.modelValue;
     },
     { immediate: true }
@@ -29,18 +41,20 @@ watch(
 </script>
 
 <template>
-    <div>
+    <div class="cursor-pointer flex items-center gap-1" @click="handleClick">
         <!-- Make a nice looking checkbox, like material design -->
-        <div class="flex items-center gap-2 text-2xl">
-            <!-- <input type="checkbox" v-model="value" class="rounded-sm border border-gray-300 w-4 h-4" /> -->
-
-            <Icon class-name="text-blue-600" name="check_box" v-if="value" />
-            <Icon name="check_box_outline_blank" v-else />
-
-            <div>
-                <slot></slot>
-            </div>
+        <div
+            :class="{
+                'flex items-center gap-2 text-2xl relative  justify-center': true,
+                [`w-[${size}] h-[${size}]`]: true,
+            }"
+        >
+            <div v-if="value" class="h-[70%] w-[70%] bg-white absolute z-0 pointer-events-none" style="left: 15%"></div>
+            <Icon :font-size="size || '1em'" class-name="text-blue-600 relative z-10" name="check_box" v-if="value" />
+            <Icon :font-size="size || '1em'" name="check_box_outline_blank" v-else />
         </div>
+
+        <slot />
     </div>
 </template>
 
