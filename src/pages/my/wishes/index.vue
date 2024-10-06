c69088762d2079e8b2ecddf6bfe1a4e9b37dc7d8
Sverre Skuland <sverre.skuland@gmail.com>
2024-10-06 18:54:11 +0200

    Move user wishes to new file.

--- src/pages/my/wishes/index.vue
+++ src/pages/my/wishes/index.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+definePageMeta({
+    pageTransition: {
+        name: 'slideLeft',
+        mode: 'out-in',
+        duration: 1000,
+    },
+});
+
 const termKey = 'MyWishes';
 const title = computed(() => i18n(termKey));
 watch(title, (newTitle) => usePageTitle(newTitle), { immediate: true });
