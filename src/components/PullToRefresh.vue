fbc2df59880506455cbf98f97fd77e6c18f27233
Sverre Skuland <sverre.skuland@gmail.com>
2025-01-02 10:32:46 +0100

    Improve user experience on pull-to-refresh.

--- src/components/PullToRefresh.vue
+++ src/components/PullToRefresh.vue
@@ -4,8 +4,15 @@ const maxPullSize = 100;
 
 const pulledHeight = ref(0);
 const reloading = ref(false);
+const pullToRefreshEnabled = ref(true);
+
+provide<Ref<boolean>>('pullToRefreshEnabled', pullToRefreshEnabled);
 
 const handleTouchMove = (e: TouchEvent) => {
+    if (!pullToRefreshEnabled.value) {
+        return;
+    }
+
     // Stop if we are not at the top of the page
     if (window.scrollY > 0 && clientYWhenTopWasHit === null) {
         pulledHeight.value = 0;
@@ -14,7 +21,7 @@ const handleTouchMove = (e: TouchEvent) => {
 
     const currentFingerPosition = e.touches[0].clientY;
     clientYWhenTopWasHit = clientYWhenTopWasHit || currentFingerPosition;
-    pulledHeight.value = Math.min(currentFingerPosition - clientYWhenTopWasHit, maxPullSize);
+    pulledHeight.value = Math.min(Math.abs(clientYWhenTopWasHit - currentFingerPosition) / 2, maxPullSize);
 };
 
 const handleTouchEnd = (e: TouchEvent) => {
@@ -38,7 +45,9 @@ const iconSize = computed(() => {
             :class="{
                 'overflow-y-hidden flex items-center justify-center absolute rounded-full bg-blue-600 z-50': true,
             }"
-            :style="`top: ${pulledHeight - 70}px; width: 50px; height: 50px; left: calc(50% - 25px);`"
+            :style="`top: ${
+                pulledHeight - 50
+            }px; width: 50px; height: 50px; left: calc(50% - 25px); transition: top 0.1s;`"
         >
             <Icon
                 :style="`transform: rotate(${260 + pulledHeight * 3}deg) scale(${iconSize}); transform-origin: center;`"
