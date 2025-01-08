d9cf95fd45aaab3faec6bdd9aa84603ba91e800d
Sverre Skuland <sverre.skuland@gmail.com>
2025-01-08 12:57:03 +0100

    Give white background to wish images that are transparent.

--- src/components/wish/Image.vue
+++ src/components/wish/Image.vue
@@ -9,7 +9,7 @@ defineProps<Props>();
 <template>
     <div
         v-if="src"
-        class="bg-cover bg-center rounded-lg overflow-hidden relative"
+        class="bg-cover bg-center rounded-lg overflow-hidden relative bg-white"
         :style="`background-image: url('${src}')`"
     ></div>
     <div v-else class="flex items-center justify-center text-4xl text-gray-400 select-none bg-white rounded-lg">?</div>
